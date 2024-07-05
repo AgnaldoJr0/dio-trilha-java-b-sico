46329152875394dfef3f11b9143f203d7df34c70
AgnaldoJr0 <113722348+AgnaldoJr0@users.noreply.github.com>
2024-07-05 02:37:04 -0300

    Mudando algumas coisas

--- TiposVariaveis/tiposVariaveis.java
+++ TiposVariaveis/tiposVariaveis.java
@@ -3,11 +3,25 @@ package TiposVariaveis;
 public class tiposVariaveis {
 
     public static void main(String[] args) {
-        byte idade = 123;
-        short ano = 2024;
-        int cep = 12410888; // se começar com 0, talvez tenha que ser outro tipo
-        long cpf = 119248191241L; // uma variavel long sempre termina com l e se começar com 0, talvez tenha que ser outro tipo
-        float pi = 3.14f; // uma variavel float sempre termina com f
-        double salario = 1500.00;
+        // byte idade = 123;
+        // short ano = 2024;
+        // int cep = 12410888; /* se começar com 0, talvez tenha que ser outro tipo */
+        // long cpf = 119248191241L; /* uma variavel long sempre termina com l e se começar com 0, talvez tenha que ser outro tipo */
+        // float pi = 3.14f; /* uma variavel float sempre termina com f */
+        // double salario = 1500.00;
+
+        // short numeroCurto = 1;
+        // int numeroNormal = numeroCurto;
+        // short numeroCurto2 = (short) numeroNormal;
+
+        // int numero = 50;
+    
+        // numero = 100;
+    
+        // System.out.print(numero);
+
+        // final double VALOR_DE_PI = 3.14;
+
+        // VALOR_DE_PI = 3.72;
     }
 }
