2e276cd5229e7a87d958b2fbf2f6fb555b02748a
AgnaldoJr0 <113722348+AgnaldoJr0@users.noreply.github.com>
2024-07-11 01:15:40 -0300

    Controle de fluxo

---
+++ Controle-Fluxo/src/CaixaEletronico.java
@@ -0,0 +1,14 @@
+public class CaixaEletronico {
+    
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double ValorSolicitado = 26.0;
+        
+        if(ValorSolicitado < saldo){
+            saldo = saldo - ValorSolicitado;
+            System.out.println("Novo saldo: " + saldo);
+        }else{
+            System.out.println("Saldo insuficiente");
+        }
+    }
+}
