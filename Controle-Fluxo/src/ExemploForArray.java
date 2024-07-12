81b17de86179f5410bbfb6f784e3c9576a9fb6b4
AgnaldoJr0 <113722348+AgnaldoJr0@users.noreply.github.com>
2024-07-12 00:57:00 -0300

    For com array

---
+++ Controle-Fluxo/src/ExemploForArray.java
@@ -0,0 +1,18 @@
+public class ExemploForArray {
+    
+    /*public static void main(String[] args) {
+        String alunos  [] = {"John", "Agnaldo", "Judas", "Julia"};
+        
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+        }
+    }*/
+
+    public static void main(String[] args) {
+        String alunos  [] = {"John", "Agnaldo", "Judas", "Julia"};
+
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+}
